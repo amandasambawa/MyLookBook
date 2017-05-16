b9c261bd509c03e59d39f8b744216fb0cb7978ad
Amanda Sambawa <amanda.sambawa@macys.com>
2017-05-16 13:48:08 -0700

    category tabs

--- src/components/CategoryTabs.jsx
+++ src/components/CategoryTabs.jsx
@@ -2,66 +2,36 @@ import React, { Component } from 'react';
 import { auth } from '../firebase.js';
 import '../styles/CategoryTabs.css';
 
-var isActive = {
-    backgroundColor: 'lightgrey'
-};
-var inactive = {
-    backgroundColor: 'white'
-};
-class CategoryTabs extends Component {
 
-    constructor() {
+class CategoryTabs extends Component {
+    constructor(){
         super();
         this.state = {
-              tops: "light-grey",
-              bottoms: "white",
-              shoes: "white",
-              accessories: "white"
+            selected:''
         };
-        this.clickTops = this.clickTops.bind(this);
+        this.setFilter = this.setFilter.bind(this);
+        this.isActive = this.isActive.bind(this);
     }
 
-    clickTops() {
-        console.log("tops clicked");
-        this.setState(
-            {
-                currentActive: "tops",
-                styles: {
-                    tops: "active",
-                    bottoms: "inactive",
-                    shoes: "inactive",
-                    accessories: "inactive"
-
-                }
-            }
-        );
+    setFilter(filter) {
+        this.setState({selected: filter});
     }
 
-
-    clickBottoms() {
-        this.setState({
-            currentActive: "bottoms",
-        })
-    }
-    clickShoes() {
-        console.log("shoes clicked");
-    }
-    clickAccessories() {
-        console.log("accessories clicked");
+    isActive(value){
+        return 'btn '+((value===this.state.selected) ?'active':'default');
     }
 
     render() {
-        return(
-            <div>
-                <div className="flex-container">
-                    <div onClick={this.clickTops} style={ {backgroundColor:this.state.tops} } className="flex-item item1">Tops</div>
-                    <div onClick={this.clickBottoms} style={ {backgroundColor:this.state.bottoms} } className="flex-item item2">Bottom</div>
-                    <div onClick={this.clickShoes} style={ {backgroundColor:this.state.shoes} } className="flex-item item3">Shoes</div>
-                    <div onClick={this.clickAccessories} style={ {backgroundColor:this.state.accessories} } className="flex-item item4">Accessories</div>
-                </div>
-            </div>
-        );
-    }
+    return (
+        <div className="flex-container">
+            <div className={'flex-item item1 ' + this.isActive('tops')} onClick={this.setFilter.bind(this, 'tops')}>Tops</div>
+            <div className={'flex-item item2 ' + this.isActive('bottoms')} onClick={this.setFilter.bind(this, 'bottoms')}>Bottoms</div>
+            <div className={'flex-item item3 ' + this.isActive('shoes')} onClick={this.setFilter.bind(this, 'shoes')}>Shoes</div>
+            <div className={'flex-item item4 ' + this.isActive('accessories')} onClick={this.setFilter.bind(this, 'accessories')}>Accessories</div>
+        </div>)
 }
+
+}
+
 
 export default CategoryTabs;
