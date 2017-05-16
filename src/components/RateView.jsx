262e7a34891891c9ab65434a7b3bf9137e02d803
Anthony Yu <anthonyyu8@gmail.com>
2017-05-16 15:12:38 -0700

    please chris

--- src/components/RateView.jsx
+++ src/components/RateView.jsx
@@ -8,34 +8,66 @@ class RateView extends Component {
     super(props);
     this.state = {
       ratingComposition: 0,
-      ratingTrendy: 0
+      ratingTrendy: 0,
+      ratingComment: "",
+      haveSaved: false
     }
       this.handleComposition = this.handleComposition.bind(this);
       this.handleTrendy = this.handleTrendy.bind(this);
       this.saveRating = this.saveRating.bind(this);
+      this.handleUsernameChange = this.handleUsernameChange.bind(this);
   }
 
+  //handles the composition rating
   handleComposition({ rating, type }) {
-    this.setState({
-      ratingComposition: rating
-    })
-    if (type === 'click') {
-      console.log('You rated Composition as ' + rating)
+    if(this.state.haveSaved === false){
+      this.setState({
+        ratingComposition: rating
+      })
+      if (type === 'click') {
+        console.log('You rated Composition as ' + rating);
+      }
+    }else{
+      console.log('You have already voted');
     }
   }
 
+  //handles the trendy rating
   handleTrendy({ rating, type }) {
-    this.setState({
-      ratingTrendy: rating
-    })
-    if (type === 'click') {
-      console.log('You rated Trendy as ' + rating)
+    if(this.state.haveSaved === false){
+      this.setState({
+        ratingTrendy: rating
+      })
+      if (type === 'click') {
+        console.log('You rated Trendy as ' + rating);
+      }
+    }else{
+      console.log('You have already voted!');
     }
   }
 
-  saveRating() {
-    console.log("WHAT DO YOU WANT FROM ME");
+  handleUsernameChange(event){
+      this.setState({ratingComment: event.target.value});
   }
+  //handles saving the rating and comments
+  saveRating() {
+    if(this.state.haveSaved === false){
+        let ratingRef = database.ref(`/users/${this.props.uid}/outfitobjects/ratings/`);
+        let newRating = ratingRef.push();
+        
+        newRating.set({
+          comment: this.state.ratingComment,
+          composition: this.state.ratingComposition,
+          trendy: this.state.ratingTrendy
+        })
+
+        this.setState({haveSaved: true});
+    } else {
+      console.log("I AM TRUE");
+    }
+  }
+
+
   render(){
     return(
       <div>
@@ -44,13 +76,13 @@ class RateView extends Component {
         <h1>Image goes here</h1>
 
         <h2>Composition</h2>
-        <Rater total={5} rating={this.state.rating} onRate={this.handleComposition} interactive={true} />
+        <Rater total={5} onRate={this.handleComposition} interactive={true} />
 
         <h2>Trendy</h2>
-        <Rater total={5} rating={this.state.rating} onRate={this.handleTrendy} interactive={true} />
+        <Rater total={5} onRate={this.handleTrendy} interactive={true} />
 
         <h2>Comment</h2>
-        <textarea placeholder="Leave a comment!"></textarea>
+        <textarea placeholder="Leave a comment!" onChange={this.handleUsernameChange}></textarea>
 
         <button onClick={this.saveRating} className="button">Save</button>
 
--- src/components/SingleOutfitView.jsx
+++ src/components/SingleOutfitView.jsx
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
 import { database } from '../firebase.js';
+import Rater from 'react-rater';
 
 class SingleOutfitView extends Component {
 
   constructor(props){
     super(props);
     this.state = {
+        compositionRating: 0,
+        trendyRating: 0
     }
   }
 
+
+  //both the ratings of these things should be a state, but for now as an
+  //example, it is random #s
   render(){
     return(
       <div>
-        SingleOutfitView
+        <h1>SingleOutfitView</h1>
+
+        <h1>Image goes here</h1>
+
+        <h2>Composition</h2>
+        <Rater interactive={false} rating={4.5} />
+
+        <h2>Trendy</h2>
+        <Rater interactive={false} rating={3.4}  />
+
+        <h2>Comment</h2>
+        <textarea>Really pretty outfit!</textarea>
+
+
       </div>
     );
   }
